253b2416b7e47e44b4f268a9069bfb031f1232d7
Ahmidi Mohamed <ahmidinador123@gmail.com>
2020-01-16 22:11:19 +0100

    .

--- UI_CSHARP/UI_CSHARP/UI_CSHARP/MainPage.xaml.cs
+++ UI_CSHARP/UI_CSHARP/UI_CSHARP/MainPage.xaml.cs
@@ -13,76 +13,99 @@ namespace UI_CSHARP
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        Button btnsign;
+        Button btnloggi;
+        Entry entr1;
+        Entry entr2;
         public MainPage()
         {
             InitializeComponent();
-            
+
+            entr1 = new Entry();
+            entr1.HorizontalOptions = LayoutOptions.CenterAndExpand;
+            entr1.Placeholder = "Email";
+            entr1.TextColor = Color.Black;
+            entr1.WidthRequest = 200;
+            entr1.HeightRequest = 40;
+            entr1.FontAttributes = FontAttributes.Bold;
+
+            entr2 = new Entry();
+            entr2.HorizontalOptions = LayoutOptions.CenterAndExpand;
+            entr2.Placeholder = "Password";
+            entr2.TextColor = Color.Black;
+            entr2.WidthRequest = 200;
+            entr2.HeightRequest = 40;
+            entr2.FontAttributes = FontAttributes.Bold;
+
+
+
+
+
             //button1 
-            var btn1 = new Button();
-            btn1.BackgroundColor = Color.Red;
-            btn1.Text = "BUTTON1";
-           //button2
-            var btn2 = new Button();
-            btn2.BackgroundColor = Color.Pink;
-            btn2.Text = "BUTTON2";
-           
-            //button3
-            var btn3 = new Button();
-            btn3.BackgroundColor = Color.Red;
-            btn3.Text = "BUTTON3";
-            //button4
-            var btn4 = new Button();
-            btn4.BackgroundColor = Color.Black;
-            btn4.TextColor = Color.White;
-            btn4.Text = "BUTTON4";
+            // var btn1 = new Button();
+            // btn1.BackgroundColor = Color.Red;
+            // btn1.Text = "BUTTON1";
+            ////button2
+            // var btn2 = new Button();
+            // btn2.BackgroundColor = Color.Pink;
+            // btn2.Text = "BUTTON2";
 
-            //column
-            var col1 = new ColumnDefinition();
-            col1.Width = 100;
-            
+            // //button3
+            // var btn3 = new Button();
+            // btn3.BackgroundColor = Color.Red;
+            // btn3.Text = "BUTTON3";
+            // //button4
+            // var btn4 = new Button();
+            // btn4.BackgroundColor = Color.Black;
+            // btn4.TextColor = Color.White;
+            // btn4.Text = "BUTTON4";
 
-            var col2 = new ColumnDefinition();
-            col2.Width = 100;
-
-            var col3 = new ColumnDefinition();
-            col3.Width = 100;
-
-            //row
-            var row1 = new RowDefinition();
-            row1.Height = 100;
-
-            var row2 = new RowDefinition();
-            row2.Height = 100;
-
-            var row3 = new RowDefinition();
-            row3.Height = 100;
-
-            var t = new Grid();
-
-            //ajouter les column en grid
-            t.ColumnDefinitions.Add(col1);
-            t.ColumnDefinitions.Add(col2);
-            t.ColumnDefinitions.Add(col3);
-            
-
-            //ajouter les row en grid
-            t.RowDefinitions.Add(row1);
-            t.RowDefinitions.Add(row2);
-            t.RowDefinitions.Add(row3);
+            // //column
+            // var col1 = new ColumnDefinition();
+            // col1.Width = 100;
 
 
-            //ajouter les boutton en grid
-            t.Children.Add(btn1,0,0);
-            t.Children.Add(btn2, 0, 1);
-            t.Children.Add(btn3, 0, 2);
-            t.Children.Add(btn4, 1, 0);
-           
+            // var col2 = new ColumnDefinition();
+            // col2.Width = 100;
+
+            // var col3 = new ColumnDefinition();
+            // col3.Width = 100;
+
+            // //row
+            // var row1 = new RowDefinition();
+            // row1.Height = 100;
+
+            // var row2 = new RowDefinition();
+            // row2.Height = 100;
+
+            // var row3 = new RowDefinition();
+            // row3.Height = 100;
+
+            // var t = new Grid();
+
+            // //ajouter les column en grid
+            // t.ColumnDefinitions.Add(col1);
+            // t.ColumnDefinitions.Add(col2);
+            // t.ColumnDefinitions.Add(col3);
+
+
+            // //ajouter les row en grid
+            // t.RowDefinitions.Add(row1);
+            // t.RowDefinitions.Add(row2);
+            // t.RowDefinitions.Add(row3);
+
+
+            // //ajouter les boutton en grid
+            // t.Children.Add(btn1,0,0);
+            // t.Children.Add(btn2, 0, 1);
+            // t.Children.Add(btn3, 0, 2);
+            // t.Children.Add(btn4, 1, 0);
+
 
             var main = new StackLayout()
             {
                 Orientation = StackOrientation.Vertical,
                 Children ={
-                   t
                 }
             };
 
