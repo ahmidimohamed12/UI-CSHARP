2e80fbc185310a5aecb74ecbaea6ab3b9c458505
Ahmidi Mohamed <ahmidinador123@gmail.com>
2020-01-16 20:09:22 +0100

    .

---
+++ Xamarin Xaml Code/Xamarin Xaml Code/Xamarin Xaml Code/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_Xaml_Code
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var main = new StackLayout()
+            {
+                Orientation = StackOrientation.Vertical,
+                Children =
+                {
+                    new Label
+                    {
+                        Text="Hello world",
+                        HorizontalOptions = LayoutOptions.CenterAndExpand,
+                        VerticalOptions = LayoutOptions.CenterAndExpand,
+                        TextColor = Color.Black
+                    },
+                    
+                } 
+            };
+            Content = main;
+        }
+    }
+}
