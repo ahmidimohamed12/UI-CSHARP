cbb0ae7e3081184e9cfb1e425fb08f668bc124e7
Ahmidi Mohamed <ahmidinador123@gmail.com>
2020-01-16 20:52:28 +0100

    .

---
+++ UI_CSHARP/UI_CSHARP/UI_CSHARP/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UI_CSHARP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            //button1 
+            var btn1 = new Button();
+            btn1.BackgroundColor = Color.Red;
+            btn1.Text = "BUTTON1";
+           //button2
+            var btn2 = new Button();
+            btn2.BackgroundColor = Color.Pink;
+            btn2.Text = "BUTTON2";
+           
+            //button3
+            var btn3 = new Button();
+            btn3.BackgroundColor = Color.Red;
+            btn3.Text = "BUTTON3";
+            //button4
+            var btn4 = new Button();
+            btn4.BackgroundColor = Color.Black;
+            btn4.TextColor = Color.White;
+            btn4.Text = "BUTTON4";
+
+            //column
+            var col1 = new ColumnDefinition();
+            col1.Width = 100;
+            
+
+            var col2 = new ColumnDefinition();
+            col2.Width = 100;
+
+            var col3 = new ColumnDefinition();
+            col3.Width = 100;
+
+            //row
+            var row1 = new RowDefinition();
+            row1.Height = 100;
+
+            var row2 = new RowDefinition();
+            row2.Height = 100;
+
+            var row3 = new RowDefinition();
+            row3.Height = 100;
+
+            var t = new Grid();
+
+            //ajouter les column en grid
+            t.ColumnDefinitions.Add(col1);
+            t.ColumnDefinitions.Add(col2);
+            t.ColumnDefinitions.Add(col3);
+            
+
+            //ajouter les row en grid
+            t.RowDefinitions.Add(row1);
+            t.RowDefinitions.Add(row2);
+            t.RowDefinitions.Add(row3);
+
+
+            //ajouter les boutton en grid
+            t.Children.Add(btn1,0,0);
+            t.Children.Add(btn2, 0, 1);
+            t.Children.Add(btn3, 0, 2);
+            t.Children.Add(btn4, 1, 0);
+           
+
+            var main = new StackLayout()
+            {
+                Orientation = StackOrientation.Vertical,
+                Children ={
+                   t
+                }
+            };
+
+            Content = main;
+        }
+    }
+}
